a30db5186ade19c5b8fc759b94201b1a31378376
Anandhan Sreekumar <anandhan.sreekumar@vonnue.com>
2024-09-07 15:24:05 +0530

    feat: module basics

---
+++ iife.js
@@ -0,0 +1,9 @@
+(function iife() {
+  const superHero = "batman";
+  console.log(superHero);
+})();
+
+(function iife() {
+  const superHero = "superman";
+  console.log(superHero);
+})();
---
+++ index.js
@@ -0,0 +1,9 @@
+const { add, sub } = require("./add");
+
+const sum = add(1, 2);
+const diff = sub(1, 2);
+
+console.log(sum);
+console.log(diff);
+
+console.log("Hello from index.js");
